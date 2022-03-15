d45e390904691d275f709b30a3a88f2a4620da8a
adam.legradi <adam.legradi@ukfast.co.uk>
2022-03-15 11:03:54 +0000

    extended calculator to allow for continuing operations

--- calculator/main.py
+++ calculator/main.py
@@ -36,19 +36,27 @@ operation_symbol = input("Pick an operation from the line above: ")
 
 num2 = int(input("What's the second number?: "))
 
-# answer = ""
-
-# while answer == "":
-#     if operation_symbol == '+':
-#         answer = add(num1, num2)
-#     elif operation_symbol == '-':
-#         answer = subtract(num1, num2)
-#     elif operation_symbol == '*':
-#         answer = multiply(num1, num2)
-#     elif operation_symbol == '/':
-#         answer = divide(num1, num2)
-
 calculation_function = operations[operation_symbol]
 answer = calculation_function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+continue_calculator = True
+
+while continue_calculator:
+
+    decision = input(f"Would you like to continue operations on the number: {answer} ? Type 'yes' to continue or 'no' to exit.\t").lower()
+    if decision == "no":
+        continue_calculator = False
+    else:
+
+        for operation in operations:
+            print(operation)
+
+        operation_symbol = input("Pick an operation from the line above: ")
+        num_next = int(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        new_answer = calculation_function(answer, num_next)
+
+        print(f"{answer} {operation_symbol} {num_next} = {new_answer}")
+        answer = new_answer
