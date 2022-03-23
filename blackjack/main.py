b5b8f27b87926aa0dcaacc9727623b04e2fb7898
Adam Legradi <adam.legradi@ukfast.co.uk>
2022-03-23 19:25:28 +0000

    cleaning up main.py for blackjack

--- blackjack/main.py
+++ blackjack/main.py
@@ -5,9 +5,7 @@ clear = lambda: os.system('clear')
 #clear()
 
 
-
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# cards = [11, 10, 10, 10]
 
 player_cards = random.choices(cards, k=2)
 cpu_cards = random.choices(cards, k=2)
@@ -26,8 +24,22 @@ def calculate_card_value(player_cards):
 
     return total_card_value
 
-# Debug into - card value
-print(calculate_card_value(player_cards))
+def show_player_card_values():
+    """Prints the cards and the value of them for the player.
+    """
+    print(f"Your cards: {player_cards}, current score: {player_cards_value}")
+
+def show_cpu_card_values():
+    """Prints the cards and the value of them for the CPU.
+    """
+    print(f"Your cards: {cpu_cards}, score: {cpu_cards_value}")
+
+def show_cpu_starting_hand():
+    cpu_shown_card = cpu_cards[0]
+    print(f"Computer's first card: {cpu_shown_card}")
+
+# Debug info - card value
+# print(calculate_card_value(player_cards))
 
 calculate_card_value(player_cards)
 
@@ -38,37 +50,46 @@ if cpu_cards_value == 21:
     print("Game Over. Computer has Blackjack!")
 elif player_cards_value == 21:
     print("Game Over. You have Blackjack!")
+else:
+    show_player_card_values()
+    show_cpu_starting_hand()
 
-def show_card_values():
-    """Prints the cards and the value of them for the player.
-    Prints the computer's first hand
-    """
-    print(f"Your cards: {player_cards}, current score: {player_cards_value}")
-    cpu_shown_card = cpu_cards[0]
-    print(f"Computer's first card: {cpu_shown_card}")
+    more_card_choice = input("Type 'y' to get another card, type 'n' to pass:\t")
+    while more_card_choice == 'y':
+        player_cards.extend(random.choices(cards))
+        player_cards_value = calculate_card_value(player_cards)
+        show_player_card_values()
+        show_cpu_starting_hand()
 
+# This part needs fixing
+        if player_cards_value > 21:
+            print("Game Over. Computer has won!")
+            more_card_choice = 'n'
+        else:
+            more_card_choice = input("Type 'y' to get another card, type 'n' to pass:\t")
+# here
 
+    cpu_more_card_choice = True
+    while cpu_more_card_choice:
+        if cpu_cards_value < 16:
+            cpu_cards.extend(random.choices(cards))
+            cpu_cards_value = calculate_card_value(cpu_cards)
+            # print(f"debug info - cpu_cards: {cpu_cards}")
+        else:
+            cpu_more_card_choice = False
 
-show_card_values()
-
-more_card_choice = input("Type 'y' to get another card, type 'n' to pass:\t")
-while more_card_choice == 'y':
-    player_cards.extend(random.choices(cards))
-    player_cards_value = calculate_card_value(player_cards)
-    show_card_values()
-    if player_cards_value > 21:
-        print("Game Over. Computer has won!")
+    if cpu_cards_value > 21:
+        print("Game Over. You have won!")
+        show_cpu_card_values()
     else:
-        more_card_choice = input("Type 'y' to get another card, type 'n' to pass:\t")
-
-cpu_more_card_choice = True
-while cpu_more_card_choice:
-    if cpu_cards_value < 16:
-        cpu_more_card_choice = True
-        cpu_cards.extend(random.choices(cards))
-        cpu_cards_value = calculate_card_value(cpu_cards)
-        print(f"debug info - cpu_cards: {cpu_cards}")
-    else:
-        cpu_more_card_choice = False
-
-
+        # Determine who has won
+        if cpu_cards_value == player_cards_value:
+            print(f"cpu_cards: {cpu_cards}")
+            print(f"player_cards: {player_cards}")
+            print("Game Over. This is a draw.")
+        elif cpu_cards_value > player_cards_value:
+            print(f"cpu_cards: {cpu_cards}")
+            print("Game Over. Computer has won!")
+        elif cpu_cards_value < player_cards_value:
+            print(f"cpu_cards: {cpu_cards}")
+            print("Game Over. You have won!")
