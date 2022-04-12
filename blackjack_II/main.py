3b3a1224cd89de9cdbf24b9d7f6d3b980eedad2a
adam.legradi <adam.legradi@ukfast.co.uk>
2022-04-12 12:43:34 +0100

    rewriting blackjack_II

--- blackjack_II/main.py
+++ blackjack_II/main.py
@@ -4,54 +4,60 @@ import os
 clear = lambda: os.system('clear')
 #clear()
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-user_cards = []
-computer_cards = []
-
-
 def deal_card():
+    """
+    Returns a random card from the deck.
+    """
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
-cards_dealt = 0
-while cards_dealt < 2:
+def calculate_score(cards):
+    # Check if Blackjack
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    # Check if gone over and one of the cards is an Ace (11)
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+def compare(score1, score2):
+    """
+    Compares the user_score and computer_score
+    """
+    
+
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+# Deal 2 cards to both computer and player
+for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
-    cards_dealt += 1
 
-print(f"user cards: {user_cards}")
-print(f"computer cards: {computer_cards}")
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
 
+    print(f"    Your cards: {user_cards}, current score: {user_score}")
+    print(f"    Computer's first card: {computer_cards[0]}")
 
-def caclulate_score(hand):
-    """
-    Calculates the value of someone's hand
-    Takes input of either user_cards or computer_cards
-    """
-    items_in_hand = len(hand)
-    score = sum(hand)
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card: ")
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
 
-    # Check if someone has Blackjack
-    if items_in_hand == 2 and score == 21:
-        score = 0
-        print("Game over! Blackjack")
-        game_over = True
-
-    # Check if someone has 11 and gone over
-    if score > 21:
-        if 11 in hand:
-            hand.remove(11)
-            hand.append(1)
-
-    # Check if the user has gone over
-    if hand == user_cards and score > 21:
-        print("Game Over! You have gone over!")
-        game_over = True
-
-    return score
-
-game_over = False
-
-print(caclulate_score(user_cards))
+while computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
 
 
