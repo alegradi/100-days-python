cb4cb1bae3898f90b03de8302ee4af0899a45d26
alegradi <adam.legradi@protonmail.ch>
2023-05-19 10:18:59 +0100

    moved some projects out of here

--- bandname_generator/main.py
+++
@@ -1,8 +0,0 @@
-# Bandname Generator
-
-print("Welcome to the bandname generator. We'll help you get a name for your band!\n")
-
-birthplace = input("Where were you born?\n")
-pet_name = input("What's your pet's name?\n")
-
-print("Your band should be called: " + birthplace + " " + pet_name)
--- odd_or_even/main.py
+++
@@ -1,9 +0,0 @@
-# Odd or even
-# Tell if a number is odd or even
-
-number = int(input("What number would you like to check? "))
-
-if number % 2 > 0:
-    print("This is an odd number!")
-else:
-    print("This is an even number!")
