9725e2198c0644a2ebf902c80528a8d1582b998c
alegradi <adam.legradi@protonmail.ch>
2023-07-24 17:17:03 +0100

    adding quiz_game

---
+++ quiz_game/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+# Testing it
+# new_question = Question("texty", "True")
+# print(new_question.text)
