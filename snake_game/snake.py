ad6be57239b9cdaca5d8261434e6ff3a713e7a28
alegradi <adam.legradi@protonmail.ch>
2023-07-31 15:41:33 +0100

    initial commit of snake_game

---
+++ snake_game/snake.py
@@ -0,0 +1,56 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            segment = Turtle(shape="square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(position)
+            self.segments.append(segment)
+
+    def move(self):
+        """
+        Making the snake move correctly
+        """
+        for seg_num in range(len(self.segments) - 1, 0, -1):  # len(segments) - 1 results in the correct index number
+            # range(start, stop, step), in this case: start 2, stop 0, step -1, so we are going 2, 1, 0
+            # @ index 2 we have the leftmost or the last segment, @ index 0 the rightmost or the first one
+
+            # we ensure that first segment 2 will go to where segment 1 is, then segment 1 to segment 0
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto((new_x, new_y))
+
+        # We then order segment 0 to move to a new location
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
