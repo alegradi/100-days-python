9db726138759a4722c43ecfabe95549c3488ef77
alegradi <adam.legradi@protonmail.ch>
2023-08-08 10:55:02 +0100

    implemented simple highscore keeping in a file

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -11,6 +11,10 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("highscore.txt", mode="r") as file:
+            stored_highescore = file.read()
+            self.highscore = int(stored_highescore)
+        # self.highscore = 0
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -18,15 +22,25 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        text = f"Score: {self.score}"
-        self.write(text, move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        text = f"GAME OVER!"
-        self.write(text, move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+
+            # Save highscore in file
+            with open("highscore.txt", mode="w") as file:
+                file.write(str(self.highscore))
+
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     text = f"GAME OVER!"
+    #     self.write(text, move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
