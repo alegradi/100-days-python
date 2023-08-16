fec1abf068dd85fe83d76f663c4e7b792fec78ad
alegradi <adam.legradi@protonmail.ch>
2023-08-16 16:36:44 +0100

    removed main_2.py, added pyperclip

--- password-manager-app/main.py
+++ password-manager-app/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -23,6 +24,7 @@ def generate_password():
 
     password_entry.delete(0, END)
     password_entry.insert(index=0, string=password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
