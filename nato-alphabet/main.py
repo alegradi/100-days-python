55ec91d82592fbb3c81ae1fae65880527c3ccf53
alegradi <adam.legradi@protonmail.ch>
2023-08-17 12:06:30 +0100

    added exception handling to user_input

--- nato-alphabet/main.py
+++ nato-alphabet/main.py
@@ -1,15 +1,22 @@
 import pandas
 
-
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(nato_alphabet)
+print(nato_alphabet)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Please enter a word: ").upper()
-output_list = [nato_alphabet[letter] for letter in user_input]
+output_list = []
 
+
+def get_user_input():
+    global output_list
+    user_input = input("Please enter a word: ").upper()
+
+    try:
+        output_list = [nato_alphabet[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the Latin alphabet please")
+        get_user_input()
+
+
+get_user_input()
 print(output_list)
