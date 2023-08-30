9ca116f6ee197b81567369e6258ea9cde3b10dad
alegradi <adam.legradi@protonmail.ch>
2023-08-30 11:33:26 +0100

    code cleanup

--- birthday-emailer/main.py
+++ birthday-emailer/main.py
@@ -55,24 +55,3 @@ if len(birthdays_today) != 0:
 
     # print(birthdays_today)  # Debug info
     email_birthday_message()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
