a68a789f0dd8000c6eb77a0ac59a1343d1465b9d
alegradi <adam.legradi@protonmail.ch>
2023-09-04 11:21:02 +0100

    testing global scope

--- doodles/scope.py
+++ doodles/scope.py
@@ -7,9 +7,15 @@ def increase_enemies():
 increase_enemies()
 print(f"enemies outside function: {enemies}")
 
+# Modifying global scope
 def increase_enemies2():
     global enemies
     enemies += 1
     print(f"enemies inside function (globally edited): {enemies}")
 
 increase_enemies2()
+
+
+# Global variable declaration
+# Convention - full capital name
+PI = 3.14159
